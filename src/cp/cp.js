718693ecea30c286ebe010002f0440056ca04b3d
ilion2221 <mr.ilion.developer@gmail.com>
2023-06-20 02:38:01 +0300

    node basic task 1 cp

--- src/cp/cp.js
+++ src/cp/cp.js
@@ -1,6 +1,15 @@
-const spawnChildProcess = async (args) => {
-    // Write your code here
+import { spawn } from 'child_process';
+import { fileURLToPath } from 'url';
+import { dirname, join } from 'path';
+
+const __modifiedDirname = dirname(fileURLToPath(import.meta.url));
+const modifiedScriptFile = join(__modifiedDirname, 'files', 'modifiedScript.js');
+
+const spawnModifiedChildProcess = async (modifiedArgs) => {
+    const modifiedChild = spawn('node', [modifiedScriptFile, ...modifiedArgs]);
+
+    process.stdin.on('data', (data) => modifiedChild.stdin.write(data));
+    modifiedChild.stdout.on('data', (data) => console.log(`Modified child process stdout:\n${data.toString()}`));
 };
 
-// Put your arguments in function call to test this functionality
-spawnChildProcess( /* [someArgument1, someArgument2, ...] */);
+spawnModifiedChildProcess(['Argument1', 'Argument2']);
